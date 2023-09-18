459ef7a3314cd6152e2a98187a91f698941cb57b
annapetrosyan1 <aptrsn@yandex.ru>
2023-09-18 17:56:05 +0500

    auth & check role middleware

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -43,7 +43,8 @@ class userController {
     }
 
     async check(req, res, next) {
-        
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return res.json({token})
     }
 }
 
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userControllers')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.check) //проверка на авторизацию
+router.get('/auth', authMiddleware, userController.check) //проверка на авторизацию
 
 module.exports = router
