2d5c1e3542369f8b0d1e1a04034cfd9bcd3daafd
annapetrosyan1 <aptrsn@yandex.ru>
2023-09-27 00:24:14 +0500

    registration & login page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,19 +4,29 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from "react-router-dom";
 import { SHOP_ROUTE } from "../utils/consts";
+import { Button, Container } from 'react-bootstrap';
+import { observer } from 'mobx-react-lite';
 
-const NavBar = () => {
+const NavBar = observer(() => {
     const {user} = useContext(Context)
     return (
         <Navbar bg="dark" data-bs-theme="dark">
-          <NavLink style={{color: "white"}} to={SHOP_ROUTE}>aptrsn shop</NavLink>
-          <Nav className="me-auto">
-            <Nav.Link href="#brands">Brands</Nav.Link>
-            <Nav.Link href="#devices">Devices</Nav.Link>
-            <Nav.Link href="#types">Types</Nav.Link>
-          </Nav>
+          <Container>
+              <NavLink style={{color: "white"}} to={SHOP_ROUTE}>aptrsn shop</NavLink>
+              {user.isAuth ?
+                  <Nav className='ms-auto' style={{color: 'white'}}>
+                    <Button variant="outline-light">Админ панель</Button>
+                    <Button variant="outline-light" className="ms-2">Войти</Button>
+                  </Nav>
+                  :
+                  <Nav className='ms-auto' style={{color: 'white'}}>
+                    <Button variant="outline-light" onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+                  </Nav>
+              }
+          </Container>
+          
       </Navbar>
     );
-};
+});
 
 export default NavBar;
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,51 @@
 import React from "react";
+import { Card, Container, Form, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { NavLink, useLocation } from "react-router-dom";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation();
+    const isLogin = location.pathname === LOGIN_ROUTE
+    console.log(location)
+
     return (
-        <div>
-            AUTH
-        </div>
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 500}} className="p-5">
+                <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control 
+                        className="mt-4"
+                        placeholder="Введите email"
+                    />
+                    <Form.Control 
+                        className="mt-3"
+                        placeholder="Введите пароль"
+                    />
+                    <Row className="d-flex justify-content-between mt-2 px-3">
+                        {isLogin ?
+                        <div>
+                            Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрироваться</NavLink>
+                        </div>
+                        :
+                        <div>
+                            Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войти</NavLink>
+                        </div>
+                        }
+                        <Button
+                            className="mt-3"
+                            variant='secondary'
+                            //onClick={ }
+                        >
+                        {isLogin ? 'Войти' : 'Зарегистрироваться'}
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
