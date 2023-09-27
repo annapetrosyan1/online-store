1452193dbbd744a7dec932a783dc5a744d9c0e47
annapetrosyan1 <aptrsn@yandex.ru>
2023-09-28 01:40:34 +0500

    device page and its characteristics

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -4,10 +4,10 @@ import { useNavigate } from 'react-router-dom';
 import { DEVICE_ROUTE } from "../utils/consts";
 
 const DeviceItem = ({device}) => {
-    const history = useNavigate()
+    const navigate = useNavigate()
 
     return (
-        <Col md={3} className="mt-3" onClick={() => history(DEVICE_ROUTE + '/' + device.id)}>
+        <Col md={3} className="mt-3" onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)}>
             <Card style={{width: 150, cursor: 'pointer'}} border="light">
                 <Image width={150} height={150} src={device.img}></Image>
                 <div className="text-black-50 mt-1 d-flex justify-content-between align-items-center">
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,63 @@
 import React from "react";
+import { Container, Col, Image, Row, Card, Button} from 'react-bootstrap';
+import bigStar from '../assets/bigStar.svg';
 
 const DevicePage = () => {
+    const device = {id: 1, name: 'Iphone 12 pro', price: 25000, rating: 5, img: 'https://cdn1.ozone.ru/s3/multimedia-n/6069408023.jpg'};
+    const description = [
+        {id: 1, title: 'Оперативная память', description: '5 гб'},
+        {id: 2, title: 'Камера', description: '12 мп'},
+        {id: 3, title: 'Процессор', description: 'Пентиум 3'},
+        {id: 4, title: 'Кол-во ядер', description: '2'},
+        {id: 5, title: 'Аккумулятор', description: '4000'},
+    ]
+
     return (
-        <div>
-            Device Page
-        </div>
+        <Container className="mt-3">
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img} />
+                </Col>
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                        <div className="d-flex justify-content-center align-items-center"
+                            style={{
+                                background: `url(${bigStar}) no-repeat center center`, 
+                                width: 240, 
+                                height: 240, 
+                                backgroundSize: 'cover',
+                                fontSize: 48,
+                            }}                    
+                        >
+                            {device.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{
+                            width: 300, 
+                            height: 300, 
+                            fontSize: 32, 
+                            border: '5px solid lightgray'
+                        }}
+                    >
+                        <h3>От: {device.price} руб.</h3>
+                        <Button variant="outline-dark">Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Характеристики</h1>
+                {description.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
     );
 };
 
